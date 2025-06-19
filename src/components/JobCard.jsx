e8ec6a3569a49c34400cf08fd61c9d0149052060
Akash <hafejmohammadakash@gmail.com>
2025-06-19 23:19:47 +0600

    data gulo job-posted compote dynamic vabe set korlam

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -1,12 +1,18 @@
 /* eslint-disable react/prop-types */
 
 import { Link } from "react-router-dom";
-import {format} from"date-fns";
+import { format } from "date-fns";
 
 const JobCard = ({ job }) => {
-  const { title, deadline, category, min_price, max_price, description } = job || {};
-console.log(title)
-  
+  const {
+    title,
+    deadline,
+    category,
+    min_price,
+    max_price,
+    description,
+  } = job || {};
+  console.log(title);
 
   return (
     <Link
@@ -15,7 +21,7 @@ console.log(title)
     >
       <div className="flex items-center justify-between">
         <span className="text-xs font-light text-gray-800 ">
-          Deadline: {format( new Date(deadline), "P")}
+          Deadline: {format(new Date(deadline), "P")}
         </span>
         <span className="px-3 py-1 text-[8px] text-blue-800 uppercase bg-blue-200 rounded-full ">
           {category}
@@ -23,12 +29,10 @@ console.log(title)
       </div>
 
       <div>
-        <h1 className="mt-2 text-lg font-semibold text-gray-800 ">
-          {title}
-        </h1>
+        <h1 className="mt-2 text-lg font-semibold text-gray-800 ">{title}</h1>
 
         <p className="mt-2 text-sm text-gray-600 ">
-          {description}...
+          {description?.substring(0, 100)}...
         </p>
         <p className="mt-2 text-sm font-bold text-gray-600 ">
           Range: ${min_price} - ${max_price}
